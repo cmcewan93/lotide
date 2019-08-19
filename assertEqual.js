4687171597b36ebdb219f61cdc4b68e96c7d1663
Colin McEwan <colin-mcewan@hotmail.com>
2019-08-19 19:05:13 +0000

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(" ✅✅✅Assertion passed: " + actual + "===" + expected) :
+    console.log(" ❌❌❌Assertion failed: " + actual + "!==" + expected);
+};
+
+assertEqual('true', 'true');
+assertEqual('true', 'false');
+assertEqual(1, 2);
+assertEqual(5, 5);
