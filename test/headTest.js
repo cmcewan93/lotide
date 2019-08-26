97d3edea29521be313831f770c97559316e31a40
Colin McEwan <colin-mcewan@hotmail.com>
2019-08-26 17:43:33 +0000

    added mocha chai test case format for head and tail

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,18 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for [5]", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
+/*assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1,2,3]), 1);
 assertEqual(head([]), undefined);
 assertEqual(head([0,' ']), 0);
+*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,23 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  const answer = ['Lighthouse', 'Labs'];
+  it('returns [Lighthouse , Labs]', () => {
+    assert.deepEqual(tail(words), answer);
+  });
+});
+
+
+
+
+
+/*const assertEqual = require('../assertEqual');
 const assertArraysEqual = require('../assertArraysEqual')
+*/
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+/*const words = ["Yo Yo", "Lighthouse", "Labs"];
 const answer = ['Lighthouse', 'Labs'];
 tail(words); // no need to capture the return value since we are not checking it
 assertArraysEqual(tail(words), answer); // original array should still have 3 elements!
@@ -11,3 +25,4 @@ assertEqual(words.length, 3);
 const test = [];
 console.log(tail(test));
 console.log(tail(words));
+*/
